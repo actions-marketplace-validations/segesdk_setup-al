f2d683d1ca401322cd5fa436e55a9f89cc58ff1a
Jasper Nygaard <jasper@sovs.net>
2022-04-20 15:54:25 +0200

    alternate syntax

--- src/Setup-AL.ps1
+++ src/Setup-AL.ps1
@@ -13,7 +13,5 @@ Import-Module $modulePath -Force
 
 $Env:PATH
 $path = (Get-Alc -version $version -alternativeDownloadUrl $alternativeDownloadUrl)[1]
-# $Env:PATH += 
-# $env:path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") + ";" + $path
-# $Env:PATH
-#refreshenv
+Write-Host "Path: $path"
+"$path" >> $env:GITHUB_PATH
