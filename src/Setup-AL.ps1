6607f4c4fee8d7ab6a3b481d74f5c670edc52380
Jasper Nygaard <jasper@sovs.net>
2022-04-20 14:42:03 +0200

    debugging env:path append

--- src/Setup-AL.ps1
+++ src/Setup-AL.ps1
@@ -13,7 +13,7 @@ Import-Module $modulePath -Force
 
 $Env:PATH
 $path = (Get-Alc -version $version -alternativeDownloadUrl $alternativeDownloadUrl)[1]
-$Env:PATH += 
-$env:path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") + ";" + $path
-$Env:PATH
-refreshenv
+# $Env:PATH += 
+# $env:path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") + ";" + $path
+# $Env:PATH
+#refreshenv
