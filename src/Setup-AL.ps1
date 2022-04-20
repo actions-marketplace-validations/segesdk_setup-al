14b5525e3f226a58b092a81b6b1b5685067fae54
Jasper Nygaard <jasper@sovs.net>
2022-04-20 11:20:54 +0200

    fixed path for alc.exe

--- src/Setup-AL.ps1
+++ src/Setup-AL.ps1
@@ -7,6 +7,6 @@ param (
 $here = Split-Path $MyInvocation.MyCommand.Definition
 $modulePath = Join-Path -Path $here -ChildPath 'AL-Lib.psm1'
 Import-Module $modulePath -Force
-$path = Get-Alc
+$path = (Get-Alc)[1]
 $Env:PATH += ";$path"
 $Env:PATH
