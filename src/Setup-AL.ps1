c9aca9f38f07314c0c75370157123577d576d2c6
Jasper Nygaard <jasper@sovs.net>
2022-04-20 12:33:41 +0200

    output env:path

--- src/Setup-AL.ps1
+++ src/Setup-AL.ps1
@@ -13,5 +13,7 @@ Import-Module $modulePath -Force
 
 $Env:PATH
 $path = (Get-Alc -version $version -alternativeDownloadUrl $alternativeDownloadUrl)[1]
-$Env:PATH += ";$path"
+$Env:PATH += 
+$env:path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") + ";" + $path
 $Env:PATH
+refreshenv
