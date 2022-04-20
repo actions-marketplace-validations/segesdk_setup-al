438115a306366800cf250229ba7d0d0f10fadce1
Jasper Nygaard <jasper@sovs.net>
2022-04-20 11:17:40 +0200

    enable download of vsix package

--- src/AL-Lib.psm1
+++ src/AL-Lib.psm1
@@ -14,22 +14,23 @@ function Get-Alc {
 	# Download destination (root of PowerShell script execution path):
 	$DownloadPathDestination = join-path (get-location) "alc"
 	
-    # if(Test-Path $DownloadPathDestination) {
-    #     Remove-Item -fo -r $DownloadPathDestination
-    # }
-    # New-Item -ItemType Directory $DownloadPathDestination
+    if(Test-Path $DownloadPathDestination) {
+        Remove-Item -fo -r $DownloadPathDestination
+    }
     
-    # $DownloadZipDestination = join-path $DownloadPathDestination "alc.zip"
-    # if(Test-Path $DownloadZipDestination) {
-    #     Remove-Item -fo $DownloadZipDestination
-    # }
+    New-Item -ItemType Directory $DownloadPathDestination
     
-    # # Download from GitHub:
-    # Invoke-WebRequest -Uri $Url -OutFile $DownloadZipDestination
+    $DownloadZipDestination = join-path $DownloadPathDestination "alc.zip"
+    if(Test-Path $DownloadZipDestination) {
+        Remove-Item -fo $DownloadZipDestination
+    }
+    
+    # Download from GitHub:
+    Invoke-WebRequest -Uri $Url -OutFile $DownloadZipDestination
 
-    # # Unzip alc portable, and then delete the zip file:
-    # Expand-Archive -Path $DownloadZipDestination -DestinationPath $DownloadPathDestination
-    # Remove-Item $DownloadZipDestination
+    # Unzip alc portable, and then delete the zip file:
+    Expand-Archive -Path $DownloadZipDestination -DestinationPath $DownloadPathDestination
+    Remove-Item $DownloadZipDestination
 
 	$alcPath = join-path $DownloadPathDestination "extension\bin\win32\"
 	write-host "path: $alcPath"
