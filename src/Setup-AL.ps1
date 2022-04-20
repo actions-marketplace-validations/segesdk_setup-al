a25274360f3034875691b04ff1a8177c4e9eee31
Jasper Nygaard <jmn@seges.dk>
2022-04-20 22:00:18 +0200

    Update Setup-AL.ps1

--- src/Setup-AL.ps1
+++ src/Setup-AL.ps1
@@ -11,7 +11,5 @@ $here = Split-Path $MyInvocation.MyCommand.Definition
 $modulePath = Join-Path -Path $here -ChildPath 'AL-Lib.psm1'
 Import-Module $modulePath -Force
 
-$Env:PATH
 $path = (Get-Alc -version $version -alternativeDownloadUrl $alternativeDownloadUrl)[1]
-Write-Host "Path: $path"
 "$path" >> $env:GITHUB_PATH
